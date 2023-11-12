dc0f12a9e55542bdcd2648158f8f3827829c8d96
danilo nunes <danilowebdev@gmail.com>
2023-11-12 20:23:09 -0300

    Initial commit

---
+++ src/main/java/org/example/DemoApplication.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWord(){
+        return applicationName;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
