52fe15aef698ef55f2328bfad8df1c60ae754fbb
danilo nunes <danilowebdev@gmail.com>
2023-11-12 22:26:14 -0300

    "Anotations"

--- src/main/java/org/example/MinhaConfiguration.java
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -1,10 +1,15 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class MinhaConfiguration {
 
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
